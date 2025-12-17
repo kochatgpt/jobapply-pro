963f356506b381360023866b1137a86c97c30aa0
SM @ KO <ai3@ko.in.th>
2025-12-17 06:58:47 +0000

    Photo and logo sizes adjusted.

--- src/components/admin/pdf/PDFLayoutType2.jsx
+++ src/components/admin/pdf/PDFLayoutType2.jsx
@@ -188,7 +188,7 @@ export default function PDFLayoutType2({ applicant }) {
 
                     {/* Photo Box */}
                     <div className="col-span-2">
-                        <div className="border border-slate-400 w-[100px] h-[150px] flex items-center justify-center bg-slate-50 relative mx-auto">
+                        <div className="border border-slate-400 w-[50px] h-[66px] flex items-center justify-center bg-slate-50 relative mx-auto">
                              {applicant.photo_url ? (
                                 <img src={applicant.photo_url} alt="Photo" className="w-full h-full object-cover absolute inset-0" />
                             ) : (
