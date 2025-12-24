cf97517d411dfca980a56b60fcb0c7f8c8d24798
SM @ KO <ai3@ko.in.th>
2025-12-24 04:15:02 +0000

    Manual code change

--- src/components/admin/pdf/PDFLayoutType2.jsx
+++ src/components/admin/pdf/PDFLayoutType2.jsx
@@ -993,9 +993,9 @@ export default function PDFLayoutType2({ applicant }) {
                         
                         <div className="border-[0.5px] border-slate-400 border-l-0 p-1.5">
                             <div className="font-bold text-center mb-1 pb-1 border-b border-slate-300">ผู้จัดการฝ่ายบุคคล</div>
-                            <div className="mb-0.5 text-center">ลงชื่อ<DottedLine className="inline-block w-24 ml-1 text-center" /></div>
-                            <div className="mb-0.5 text-center">(<DottedLine className="inline-block w-full text-center" />)</div>
-                            <div className="mb-1 text-center">วันที่<DottedLine className="inline-block w-24 ml-1 text-center" /></div>
+                            <div className="mb-0.5 text-center">ลงชื่อ<DottedLine className="inline-block w-32 ml-1 text-center" /></div>
+                            <div className="mb-0.5 text-center">(<DottedLine className="inline-block w-32 text-center" />)</div>
+                            <div className="mb-1 text-center">วันที่<DottedLine className="inline-block w-32 ml-1 text-center" /></div>
                         </div>
 
                         {/* Second Row - 2 columns */}
