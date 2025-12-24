dcf60314732d1f45cd06362b7f70452e136a50c9
SM @ KO <ai3@ko.in.th>
2025-12-24 04:56:36 +0000

    Manual code change

--- src/components/admin/pdf/PDFLayoutType2.jsx
+++ src/components/admin/pdf/PDFLayoutType2.jsx
@@ -975,9 +975,9 @@ export default function PDFLayoutType2({ applicant }) {
                         <div className="border-[0.5px] border-slate-400 p-1.5">
                             <div className="font-bold text-center mb-1 pb-1 border-b border-slate-300">เจ้าหน้าที่สรรหาว่าจ้าง</div>
                             <div className="text-center mb-4">ผู้ดำเนินการ</div>
-                            <div className="mb-0.5 text-center">ลงชื่อ<DottedLine className="inline-block w-42 ml-1 relative top-[1px]" /></div>
-                            <div className="mb-0.5 text-center">(<DottedLine className="inline-block w-42 ml-1 relative top-[1px]" />)</div>
-                            <div className="mb-1 text-center">วันที่<DottedLine className="inline-block w-42 ml-1 relative top-[1px]" /></div>
+                            <div className="mb-0.5 text-center">ลงชื่อ<DottedLine className="inline-block w-42 ml-1" /></div>
+                            <div className="mb-0.5 text-center">(<DottedLine className="inline-block w-42 ml-1" />)</div>
+                            <div className="mb-1 text-center">วันที่<DottedLine className="inline-block w-42 ml-1" /></div>
                         </div>
                         
                         <div className="border-[0.5px] border-slate-400 border-l-0 p-1.5">
