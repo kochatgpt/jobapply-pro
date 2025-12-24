f952c70f9bd16b4274dd89ac863a2012f852dfb5
SM @ KO <ai3@ko.in.th>
2025-12-24 06:19:54 +0000

    Manual code change

--- src/components/admin/pdf/PDFLayoutType2.jsx
+++ src/components/admin/pdf/PDFLayoutType2.jsx
@@ -540,7 +540,7 @@ export default function PDFLayoutType2({ applicant }) {
             </PageContainer>
 
             {/* ================= PAGE 3 ================= */}
-            <PageContainer pageNum={3} totalPages={4}>
+            <PageContainer pageNum={3} totalPages={4} logoTopOffset="top-[-10px]">
                 <div className="mt-[5mm] space-y-1">
 
                      {/* --- Work History --- */}
