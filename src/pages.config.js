fe2cbfb040df76693daca3a5e2c2599a43690d16
SM @ KO <ai3@ko.in.th>
2026-01-05 03:03:33 +0000

    Cleaned PDF generation code

--- src/pages.config.js
+++ src/pages.config.js
@@ -2,13 +2,13 @@ import Admin from './pages/Admin';
 import Application from './pages/Application';
 import Home from './pages/Home';
 import PDFPreview from './pages/PDFPreview';
+import criminalCheckForm from './pages/criminal-check-form';
 import employmentContract from './pages/employment-contract';
 import fmHrd19 from './pages/fm-hrd-19';
 import fmHrd27 from './pages/fm-hrd-27';
 import pdpaForm from './pages/pdpa-form';
-import userLogin from './pages/user-login';
 import userDashboard from './pages/user-dashboard';
-import criminalCheckForm from './pages/criminal-check-form';
+import userLogin from './pages/user-login';
 import __Layout from './Layout.jsx';
 
 
@@ -17,13 +17,13 @@ export const PAGES = {
     "Application": Application,
     "Home": Home,
     "PDFPreview": PDFPreview,
+    "criminal-check-form": criminalCheckForm,
     "employment-contract": employmentContract,
     "fm-hrd-19": fmHrd19,
     "fm-hrd-27": fmHrd27,
     "pdpa-form": pdpaForm,
-    "user-login": userLogin,
     "user-dashboard": userDashboard,
-    "criminal-check-form": criminalCheckForm,
+    "user-login": userLogin,
 }
 
 export const pagesConfig = {
