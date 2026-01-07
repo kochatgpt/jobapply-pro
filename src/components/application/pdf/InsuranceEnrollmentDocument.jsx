43ff62d4812b8fef3f543bcb127fb9d281363706
SM @ KO <ai3@ko.in.th>
2026-01-07 04:46:39 +0000

    Manual code change

--- src/components/application/pdf/InsuranceEnrollmentDocument.jsx
+++ src/components/application/pdf/InsuranceEnrollmentDocument.jsx
@@ -212,7 +212,7 @@ export default function InsuranceEnrollmentDocument({ applicant, formData = {} }
                             <p className="text-[14px] italic">If you are provided with dependent coverage and are enrolling your dependents, please complete a "Dependent Enrollment Form".</p>
                         </div>
                     </div>
-                    <div className="grid-col-5 gap-6 justify-center">
+                    <div className="grid grid-col-5 gap-6 justify-center">
                         <div className="text-center col-span-2">
                             <div className="border-b border-slate-400 min-h-[50px] mb-2 mx-4 flex items-center justify-center">
                                 {formData.signatureDate ? new Date(formData.signatureDate).toLocaleDateString('th-TH', { 
